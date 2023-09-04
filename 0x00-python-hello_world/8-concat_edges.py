2cfd45dad4b0ef7d8635e486ae24fb9d4e07ea04
kelvinnmuia <kelvinnmuia@gmail.com>
2023-09-05 02:26:12 +0300

    python script to slice a str

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[42:66] + str[114:119] + str[0:6])
+str = str[42:66] + str[114:119] + str[:6]
+print(str)

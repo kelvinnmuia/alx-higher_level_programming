5cd1cf8ab3188ee6aa068a4087b29c0774a77fc9
kelvinnmuia <kelvinnmuia@gmail.com>
2023-09-05 17:29:34 -0700

    python function to determine power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    output = 1
-
-    if b >= 0:
-        for _ in range(b):
-            output *= a
-    else:
-        for _ in range(-b):
-            output /= a
-    return output 
+    return (a**b)

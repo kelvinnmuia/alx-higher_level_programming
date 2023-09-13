585d95278de7a792c2b3ed747c4d96e38c073ad3
kelvinnmuia <kelvinnmuia@gmail.com>
2023-09-13 13:45:11 -0700

    python program to compute matrix squares

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix[]):
-    return ([list(map(lambda num: num * num, r)) for r in matrix])
+    return ([list(map(lambda n: n * n, row)) for row in matrix])

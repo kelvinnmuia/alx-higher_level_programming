abebee9402de320a12242424c8f65924dd897f9e
kelvinnmuia <kelvinnmuia@gmail.com>
2023-09-25 15:55:18 -0700

    raise exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""function that raises exception"""
+
+
+def raise_exception():
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""function that raises exception message"""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)

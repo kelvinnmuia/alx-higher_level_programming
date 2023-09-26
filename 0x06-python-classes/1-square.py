b2d202812c8d70a60e512da8cd4b5825597dfbf9
kelvinnmuia <kelvinnmuia@gmail.com>
2023-09-26 11:16:44 -0700

    creating a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this an empty class to define square"""
+
+
+class Square:
+    """the class to define square"""
+    def __init__(self, size):
+        """initialize the object parameters
+
+        Args:
+        size(int): the size of the square objects
+        """
+        self._size = size

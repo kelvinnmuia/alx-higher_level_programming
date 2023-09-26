955a954000686718a26cd44064f6d6477838ab28
kelvinnmuia <kelvinnmuia@gmail.com>
2023-09-26 13:07:50 -0700

    created a getter and setter for square class instances

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,12 +10,18 @@ class Square:
         Args:
         size(int): the size of the square objects
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
     def area(self):
         """public instance method to return current area of the square"""

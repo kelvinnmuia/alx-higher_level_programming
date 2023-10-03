b3749006f2772cc7dcf0cf79dda3c93f49ce0e9b
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-03 07:34:31 -0700

    everything is an object in python

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,5 +2,6 @@
 class LockedClass:
     def __setattr__(self, name, value):
         if name != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+            raise AttributeError("'LockedClass' object has no attribute '{}'"
+                                 .format(name))
         super().__setattr__(name, value)

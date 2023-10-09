3d7f83fd3340572d1cf19f09b32d66acfa369601
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-09 12:49:48 -0700

    added str() and print() to the Square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" defines the square class that inherits
+Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Rectangle class that inherits BaseGeometry with print() and str()"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self._size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return (self._size ** 2)
+
+    def __str__(self):
+        str_rp = "[Square] " + str(self._size) + "/" + str(self._size)
+        return str_rp

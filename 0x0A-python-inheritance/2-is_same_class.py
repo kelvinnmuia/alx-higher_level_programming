5209e3f19165d91d9db97ce97899dd30a4d38f64
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-09 13:54:16 -0700

    created function to set attributes to an object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,7 +2,10 @@
 """ defines function that returns true when the
 object is exactly the same as the specified class"""
 
+
 def is_same_class(obj, a_class):
+    """ returns True when object is exactly
+    the same as the specified class"""
     if type(obj) is a_class:
         return True
     else:

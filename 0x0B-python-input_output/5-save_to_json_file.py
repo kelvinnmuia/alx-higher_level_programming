81af32320dddf9b7533d1b6403356ff4cc93781b
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-10 13:26:22 -0700

    python function to save file as JSON

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
-#!/user/bin/python3
-"""defines function that write JSON
-representain of an object to file"""
+#!/usr/bin/python3
+"""defines function to write JSON representatin of an object to file"""
 
 
 def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+    """write JSON representation of an object to a file"""
+    import json
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj, ensure_ascii=False))

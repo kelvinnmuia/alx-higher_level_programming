eeb8576e5c52b1354ff500ca31d02bdc0b4fb4d4
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-10 12:23:19 -0700

    created python functions to handle JSON

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defines function that writes string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """writes string to text file"""
+    with open(filename, 'a') as f:
+        return f.write(text)

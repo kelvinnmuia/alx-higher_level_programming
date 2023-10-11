625d3640d04081c057b7421c9af37270399bcb30
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-11 09:16:51 -0700

    created python script to add item to json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,13 +7,9 @@ from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 filename = "add_item.json"
 with open(filename, 'a+', encoding="utf-8") as f:
-    if exists(filename)
-        my_list = load_from_json_file(filename)
-    else:
-        my_list = []
-
+    my_list = []
     my_list.extend(sys.argv[1:])
     save_to_json_file(my_list, filename)
+    load_from_json_file(filename)

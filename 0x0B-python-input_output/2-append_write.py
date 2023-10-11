70cf4217bef6f6168c5199c1ac6df07dca1e2abc
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-11 09:22:21 -0700

    created python script to add item to json file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """ defines function that writes string to a text file"""
 
 
-def write_file(filename="", text=""):
+def append_file(filename="", text=""):
     """writes string to text file"""
     with open(filename, 'a') as f:
         return f.write(text)

b4859f23fb9f67bed2c72dc58df1c9a9bf54bc11
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-16 14:03:19 -0700

    created the square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""defines the Square class"""
+"""defines the Square class which inherits from Rectangle"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class for square instances"""
-
+    """class for the Square instances"""
     def __init__(self, size, x=0, y=0, id=None):
-    """instantiates the Square class"""
-    super().__init__(size, size, x, y, id)
-    self.size = size
+        """initialize the Square instance"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
-        """getter for the private instance attribute size"""
+        """getter method for size"""
         return (self.width)
 
-    @size.setter(self, value):
+    @size.setter
     def size(self, value):
         """setter for the private instance attribute size"""
         if type(value) is not int:
@@ -30,39 +29,7 @@ class Square(Rectangle):
             self.height = value
 
     def __str__(self):
-        """overides the __str__ method with new string format"""
+        """overrides the __str__ with new string format"""
         str_rp = "[Square] ({}) {}/{} - {}".format(
-                str(self.id), str(self.x), str(self.y), str(self.width))
+            str(self.id), str(self.x), str(self.y), str(self.size))
         return (str_rp)
-
-    def update(self, *args, **kwargs):
-        """assigns an argument to each Square attribute"""
-        if args and len(args) != 0:
-            for i in range(len(args)):
-                if i == 0:
-                    self.id = args[i]
-                if i == 1:
-                    self.size = args[i]
-                if i == 2:
-                    self.x = args[i]
-                if i == 3:
-                    self.y = args[i]
-        else:
-            for kw in kwargs:
-                if kw == "id":
-                    self.id = (kwargs[kw])
-                if kw == "size":
-                    self.size(kwargs[kw)
-                if kw == "x":
-                    self.x = (kwargs[kw])
-                if kw == "y":
-                    self.y = (kwargs[kw])
-
-    def to_dictionary(self):
-        """returns dictionary representation of a Square"""
-        dict_rp = {}
-        dict_rp["id"] = self.id
-        dict_rp["size"] = self.size
-        dict_rp["x"] = self.x
-        dict_rp["y"] = self.y
-        return dict_rp

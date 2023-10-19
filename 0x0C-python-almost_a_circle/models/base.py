099f74528e2cd4c0ccada4d03887d7d65580fd59
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-19 07:46:48 -0700

    created a class method that saves json string to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,10 +16,24 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns JSON string representation of the list of dictionaries"""
+        """returns JSON string representation
+        of the list of dictionaries"""
         import json
         if list_dictionaries is None:
             return "[]"
         if len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list
+        of instances who inherists from Base to a file"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        list_dictionaries = []
+        for instance in list_objs:
+            list_dictionaries.append(instance.to_dictionary())
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_dictionaries))

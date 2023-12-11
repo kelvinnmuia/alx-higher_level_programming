d59a4112e9791aa1f5e332b1b3b6f3d867316b23
kelvinnmuia <kelvinnmuia@gmail.com>
2023-12-11 12:30:27 -0800

    created a script in javascript that prints a string from an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let i = 0;
-for (i = 0; i < myVar.length; i++) {
-  console.log(myVar[i]);
+for (i = 0; i < arrVar.length; i++) {
+  console.log(arrVar[i]);
 }

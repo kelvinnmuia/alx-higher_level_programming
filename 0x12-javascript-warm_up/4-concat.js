29b1ab1f9a6c2a7446727c1a318c50e80c7f0b71
kelvinnmuia <kelvinnmuia@gmail.com>
2023-12-11 12:01:04 -0800

    refactored the script

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-const printString = myArgs[0] + ' is ' + myArgs[1];
-console.log(printString);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);

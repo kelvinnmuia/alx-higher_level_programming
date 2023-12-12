694dacfd0796c68511dd467009156fcce54c3ace
kelvinnmuia <kelvinnmuia@gmail.com>
2023-12-12 11:11:42 -0800

    refactored the code in 101-sorted.js to be semistandard compliant

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let nwDict = {};
-for (let key in dict) {
+const nwDict = {};
+for (const key in dict) {
   if (nwDict[dict[key]] === undefined) {
     nwDict[dict[key]] = [key];
   } else {

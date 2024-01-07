1aea0353d291ed68d587d5b46f85777d171bddb8
kelvinnmuia <kelvinnmuia@gmail.com>
2024-01-07 02:34:31 -0800

    created a python script to post to a URL with a given email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address, sends a POST request to the 
-passed URL with the email as a parameter, and displays the body of the response.
+Sends a POST request to the passed URL with the email as
+a parameter, and displays the body of the response.
 
 Usage: ./6-post_email.py <URL> <email>
  - Displays the body of the response.

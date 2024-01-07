475e6cf730b621f347b46cfedd363bdfaea97d55
kelvinnmuia <kelvinnmuia@gmail.com>
2024-01-07 03:36:15 -0800

    created a python script to POST a request with a letter to a URL

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+ sends a POST request to http://0.0.0.0:5000/search_user
+ with the letter as a parameter.
+
+ Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
